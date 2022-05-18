d1a40bd51d5196a245253bd619f2ee8df096582f
Roger Santos <roger.santos36@gmail.com>
2022-05-18 16:04:43 -0300

    fix: menu bars animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('[data-js="menu"]')
 const menuToggle = document.querySelector('[data-js="menu-toggle"]')
 const menuClose = document.querySelector('[data-js="menu-close"]')
+const menuList = document.querySelector('[data-js="menu-list"]')
 
 if (menuToggle) {
   menuToggle.addEventListener('click', () => {
@@ -13,3 +14,26 @@ if (menuClose) {
     menu.classList.remove('show-menu')
   })
 }
+
+// close nav bar when link is clicked
+menuList.addEventListener('click', () => {
+  menu.classList.remove('show-menu')
+})
+
+
+// stricky menu with scrolling
+
+const scrollHeader = () => {
+  const header = document.querySelector('[data-js="header"]')
+  const nav = document.querySelector('[data-js="nav"]')
+
+  if (window.scrollY >= 25) {
+    header.classList.add('scroll-header')
+    nav.classList.add('scroll-nav')
+  } else {
+    header.classList.remove('scroll-header')
+    nav.classList.remove('scroll-nav')
+  }
+}
+
+window.addEventListener('scroll', scrollHeader)
